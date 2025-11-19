26d5b38187c68c535b9cac162d7ca5bb9591d8ab
khaled88-kaboub <khaledkaboub88@gmail.com>
2025-11-19 20:52:39 +0100

    Update RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -19,7 +19,7 @@ export default function RegisterPage() {
     setSuccess("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", form);
+      const res = await axios.post("https://fixme-1.onrender.com/api/auth/register", form);
 
       if (res.status === 201) {
         setSuccess("Inscription réussie ! Redirection...");
