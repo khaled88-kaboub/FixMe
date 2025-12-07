b7bef49c96503b1056bd21d1966971fbe959242a
kaboub khaled <khaledkaboub88@gmail.com>
2025-12-07 22:09:59 +0100

    my first prod

--- client/client/src/pages/InterventionListPage.jsx
+++
@@ -1,9 +0,0 @@
-export default function InterventionListPage() {
-    return (
-      <div style={{ padding: "20px" }}>
-        <h2>📋 Liste des interventions</h2>
-        <p>Cette page affichera toutes les interventions enregistrées.</p>
-      </div>
-    );
-  }
-  
