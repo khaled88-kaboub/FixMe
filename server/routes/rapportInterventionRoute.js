a86089069ef1b16d95955717abb71cb27192bfbe
khaled88-kaboub <khaledkaboub88@gmail.com>
2026-02-02 21:53:56 +0100

    Update rapportInterventionRoute.js

--- server/routes/rapportInterventionRoute.js
+++ server/routes/rapportInterventionRoute.js
@@ -5,16 +5,20 @@ import {
   getRapportById,
   updateRapport,
   deleteRapport,
-  getTotalDuree
+  getTotalDuree,
+  getRapportsByInterventionId
 } from "../controllers/rapportInterventionController.js";
 
 const router = express.Router();
 
 router.post("/", createRapport);          // ➕ Créer
 router.get("/", getAllRapports);          // 📋 Liste
+router.get("/intervention/:interventionId", getRapportsByInterventionId);
 router.get("/:id", getRapportById);       // 🔍 Détail
 router.put("/:id", updateRapport);        // ✏️ Modifier
 router.delete("/:id", deleteRapport);     // ❌ Supprimer
 router.get("/:id/duree", getTotalDuree);  // ⏱️ Durée totale
 
+
 export default router;
+
