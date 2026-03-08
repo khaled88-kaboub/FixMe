a1b7267e6a397668c770407bb26c0f60b33b24b6
kaboub khaled <khaledkaboub88@gmail.com>
2026-03-08 23:53:06 +0100

    treu

--- client/src/api/maintenancePreventiveApi.js
+++ client/src/api/maintenancePreventiveApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API_URL = import.meta.env.VITE_API_URL;
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/maintenance-preventive"
+  baseURL: `${API_URL}/api/maintenance-preventive`
 });
 
 export const getAllMP = () => API.get("/");
